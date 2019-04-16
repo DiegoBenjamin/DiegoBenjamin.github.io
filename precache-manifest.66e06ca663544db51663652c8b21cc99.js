2d1dda079c5f179d81a3d492e5c8c048b65046e7
Diego Hernandez <dhernandez@grupoeco.com.mx>
2019-04-16 16:02:01 -0700

    primer commit

---
+++ precache-manifest.66e06ca663544db51663652c8b21cc99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82242e2a3a46f496b6ca",
+    "url": "/static/js/main.74207922.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "82242e2a3a46f496b6ca",
+    "url": "/static/css/main.903e1eeb.chunk.css"
+  },
+  {
+    "revision": "4bf2e42e9a408593d5442948e458e3e2",
+    "url": "/index.html"
+  }
+];
